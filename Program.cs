a475af7d72aa45135276d7f8ed57ee2be674a4fd
Brian Russell <Brianrussell2017@yahoo.com>
2024-12-10 16:16:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Assignment_6_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4};
+            int[] result = Solution.productExceptSelf(nums, nums.Length);
+
+            foreach (int val in result)
+            {
+                Console.Write(val + " ");
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_2_3
+{
+    public class Solution
+    {
+        public static int[] productExceptSelf(int[] nums, int n)
+        {
+            int c = 0;
+            int prod = 1;
+            int[] res = new int[n];
+
+            // Calculate product of all non-zero elements and count zeros
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == 0)
+                    c++;
+                else
+                    prod *= nums[i];
+            }
+
+            // Generate the result array
+            for (int i = 0; i < n; i++)
+            {
+                if (c > 1)
+                    res[i] = 0;
+                else if (c == 1)
+                {
+                    if (nums[i] == 0)
+                        res[i] = prod;
+                    else
+                        res[i] = 0;
+                }
+                else
+                {
+                    res[i] = prod / nums[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
